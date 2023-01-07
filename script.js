794f0cc3e90b671bb50285479708e9c7e3828723
Prince Olubari <olubari.prince.g@gmail.com>
2023-01-07 21:43:34 +0000

    added some if statements to the javascript code and some while loops to ensure that the user confirms the right character type and password length

--- script.js
+++ script.js
@@ -1,143 +1,172 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-let passwordArray=[];
-let passwordLen=0;
+let passwordArray = [];
+let passwordLen = 0;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
-  passwordLen = prompt("How long will you want your password to be?")
-if(confirm(" Would you like to use lowercase letters? ")){
-  passwordArray=passwordArray.concat(lowerCasedCharacters);
-} 
-if(confirm(" Would you like use special characters? ")){
-  passwordArray=passwordArray.concat(specialCharacters);
-}
-if(confirm("Would you like to use numeric characters ?")){
-passwordArray=passwordArray.concat(numericCharacters);
-}
-if(confirm("Would you like to use Uppercased letters ?")){
-  passwordArray=passwordArray.concat(upperCasedCharacters);
-}
+  passwordLen = prompt("How long will you want your password to be?");
+  if (passwordLen >= 10 && passwordLen <= 64) {
+    let lowerCaseConfirm = confirm(
+      " Would you like to use lowercase letters? "
+    );
+    let upperCaseCofirm = confirm(" Would you like to use UpperCase letters? ");
+    let specialCharactersConfirm = confirm(
+      " Would you like to use Special Characters ? "
+    );
+    let numericCharacterConfirm = confirm(
+      " Would you like to use numeric characters? "
+    );
+    while (
+      !lowerCaseConfirm &&
+      !upperCaseCofirm &&
+      !specialCharactersConfirm &&
+      !numericCharacterConfirm
+    ) {
+      alert("You need to select atleast one charcater type");
+      lowerCaseConfirm = confirm(" Would you like to use lowercase letters? ");
+      upperCaseCofirm = confirm(" Would you like to use UpperCase letters? ");
+      specialCharactersConfirm = confirm(
+        " Would you like to use Special Characters ? "
+      );
+      numericCharacterConfirm = confirm(
+        " Would you like to use numeric characters? "
+      );
+    }
+    if (lowerCaseConfirm) {
+      passwordArray = passwordArray.concat(lowerCasedCharacters);
+    }
+    if (specialCharacters) {
+      passwordArray = passwordArray.concat(specialCharacters);
+    }
+    if (numericCharacters) {
+      passwordArray = passwordArray.concat(numericCharacters);
+    }
+    if (upperCasedCharacters) {
+      passwordArray = passwordArray.concat(upperCasedCharacters);
+    }
+  } else if (passwordLen < 10 || passwordLen > 64 || isNaN(passwordLen)) {
+    alert(
+      "The password length has to be at least 10 characters but no more than 64."
+    );
+    getPasswordOptions();
+  }
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+function getRandom(arr) {}
 
 // Function to generate password with user input
 function generatePassword() {
- let password = "";
- 
+  let password = "";
+
   getPasswordOptions();
 
   for (let i = 0; i < passwordLen; i++) {
-    password += passwordArray[Math.floor(Math.random()*passwordArray.length)];
-    
+    password += passwordArray[Math.floor(Math.random() * passwordArray.length)];
   }
-   
+
   return password;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
